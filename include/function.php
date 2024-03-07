caf567f063964f9107f3a7dd411cd1396c72250f
theshubz <157612314+theshubz@users.noreply.github.com>
2024-03-07 22:04:29 +0530

    update

--- include/function.php
+++ include/function.php
@@ -36,7 +36,7 @@
 	}
 	function __autoload_($class_name) {
 		$class_name = strtolower($class_name);
-		$path = LIB_PATH.DS."{$class_name}.php";
+		$path = "{$class_name}.php";
 		if(file_exists($path)){
 			require_once($path);
 		}else{
